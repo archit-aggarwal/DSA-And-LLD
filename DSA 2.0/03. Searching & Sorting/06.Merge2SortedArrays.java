ee554b4e2e4a87f8b28ef6bea65385c92daabd7d
archit-aggarwal <archit.aggarwal023@gmail.com>
2022-10-16 10:06:25 +0530

    Added Sorting Codes

--- DSA 2.0/03. Searching & Sorting/06.Merge2SortedArrays.java
+++ DSA 2.0/03. Searching & Sorting/06.Merge2SortedArrays.java
@@ -34,3 +34,34 @@ class Solution1 {
         }
     }
 }
+
+// Time = O(n1 + n2), Space = O(1) Constant Extra Space
+class Solution2 {
+    public void merge(int[] nums1, int n1, int[] nums2, int n2) {
+        int ptr1 = n1 - 1, ptr2 = n2 - 1, ptr3 = n1 + n2 - 1;
+
+        while (ptr1 >= 0 && ptr2 >= 0) {
+            if (nums1[ptr1] > nums2[ptr2]) {
+                nums1[ptr3] = nums1[ptr1];
+                ptr1--;
+                ptr3--;
+            } else {
+                nums1[ptr3] = nums2[ptr2];
+                ptr2--;
+                ptr3--;
+            }
+        }
+
+        while (ptr1 >= 0) {
+            nums1[ptr3] = nums1[ptr1];
+            ptr1--;
+            ptr3--;
+        }
+
+        while (ptr2 >= 0) {
+            nums1[ptr3] = nums2[ptr2];
+            ptr2--;
+            ptr3--;
+        }
+    }
+}
