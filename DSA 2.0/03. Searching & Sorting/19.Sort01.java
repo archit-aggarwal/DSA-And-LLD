31cc9f8ca7f5b8eb154de2f697d8b520c514ed7c
archit-aggarwal <archit.aggarwal023@gmail.com>
2022-10-29 11:15:23 +0530

    Added Partitioning

---
+++ DSA 2.0/03. Searching & Sorting/19.Sort01.java
@@ -0,0 +1,23 @@
+// https://practice.geeksforgeeks.org/problems/binary-array-sorting-1587115620/1
+// Time = O(N), Space = O(1)
+// Single Traversal Approach & Inplace Approach
+class Solution {
+    static void swap(int[] arr, int left, int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+
+    static void binSort(int arr[], int N) {
+        int left = 0, right = 0;
+        while (right < N) {
+            if (arr[right] == 0) {
+                swap(arr, left, right);
+                left++;
+                right++;
+            } else {
+                right++;
+            }
+        }
+    }
+}
