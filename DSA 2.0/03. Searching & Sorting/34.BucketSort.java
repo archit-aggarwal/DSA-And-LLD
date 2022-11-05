8468beba2459b2f9348c19799649b4e82e6ade86
archit-aggarwal <archit.aggarwal023@gmail.com>
2022-11-05 11:43:06 +0530

    Added Bucket Sort

---
+++ DSA 2.0/03. Searching & Sorting/34.BucketSort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    @SuppressWarnings("all")
+    public int[] sortArray(int[] nums) {
+        int max = 50000, min = -50000; // according to constraint
+
+        // 1. Fill the Buckets Array
+        ArrayList<Integer>[] buckets = new ArrayList[max - min + 1];
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+
+        for (int val : nums)
+            buckets[val - min].add(val);
+
+        // 2. Fill the Result Sorted Array
+        int[] res = new int[nums.length];
+        int idx = 0;
+
+        for (int actualIdx = 0; actualIdx < buckets.length; actualIdx++) {
+            for (Integer val : buckets[actualIdx]) {
+                res[idx++] = val;
+            }
+        }
+
+        return res;
+    }
+}
