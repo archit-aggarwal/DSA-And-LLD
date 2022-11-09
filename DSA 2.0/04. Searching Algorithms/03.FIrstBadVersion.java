8b4043e25a0724690eecd2cdc156f843c01ecc24
archit-aggarwal <archit.aggarwal023@gmail.com>
2022-11-09 23:43:39 +0530

    ADDED BS

---
+++ DSA 2.0/04. Searching Algorithms/03.FIrstBadVersion.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/first-bad-version/description/
+// Time = O(Log2 N), Space = O(1)
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid) == false)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return left;
+    }
+}
