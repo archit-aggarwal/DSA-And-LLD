f7a6bf9a31b47851a78968ece3c5597c1d817a51
archit-aggarwal <archit.aggarwal023@gmail.com>
2022-11-14 19:20:24 +0530

    Added Multiple Interface

--- LLD or OOAD/01. Core Java Basics/29.MultipleInterfaces.java
+++ LLD or OOAD/01. Core Java Basics/29.MultipleInterfaces.java
@@ -16,10 +16,37 @@ class Car implements MyCar {
     }
 }
 
+interface Radio2 {
+    String data = "Radio"; // static
+
+    void start();
+}
+
+interface MyCar2 {
+    String data = "Car"; // static
+
+    void start();
+}
+
+class Car2 implements Radio2, MyCar2 {
+    @Override
+    public void start() {
+        System.out.println("Radio & Car Starts Together");
+    }
+
+    public void fun() {
+        System.out.println(Radio2.data);
+        System.out.println(MyCar2.data);
+    }
+}
+
 class Driver {
     public static void main(String[] args) {
         Car i10 = new Car();
         i10.drive();
         i10.playRadio();
+
+        Car2 i20 = new Car2();
+        i20.start();
     }
 }
