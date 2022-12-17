ff07f55aeef8821b3bc56a7368e2ab39f5293f63
archit-aggarwal <archit.aggarwal023@gmail.com>
2022-12-17 15:33:39 +0530

    Added Codes

--- DSA 2.0/06. Linked List/06.DeleteNodeWOHead.java
+++ DSA 2.0/06. Linked List/06.DeleteNodeWOHead.java
@@ -1,14 +1,15 @@
 // https://leetcode.com/problems/delete-node-in-a-linked-list/description/
 // LC 237: Time = O(1), Space = O(1)
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) { val = x; }
- * }
- */
+// Definition for singly-linked list
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
 
 class Solution {
     public void deleteNode(ListNode node) {
