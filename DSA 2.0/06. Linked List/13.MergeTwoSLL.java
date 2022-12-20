988d018e3f76a622c1f399dbd5948d61f1223177
archit-aggarwal <archit.aggarwal023@gmail.com>
2022-12-20 23:50:34 +0530

    Added Codes

---
+++ DSA 2.0/06. Linked List/13.MergeTwoSLL.java
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/merge-two-sorted-lists/description/
+// Time = O(N1 + N2), Space = O(1) Inplace
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                tail = tail.next;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                tail = tail.next;
+                list2 = list2.next;
+            }
+        }
+
+        if (list1 != null)
+            tail.next = list1;
+        else
+            tail.next = list2;
+        return dummy.next;
+    }
+}
