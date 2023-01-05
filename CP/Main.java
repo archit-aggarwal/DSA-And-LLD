7a04e40bb5c5bfb3ba11a9fcab3a53a42b01d663
archit-aggarwal <archit.aggarwal023@gmail.com>
2023-01-05 22:41:00 +0530

    Added Codes

---
+++ CP/Main.java
@@ -0,0 +1,102 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+    static FastReader in = new FastReader();
+    static FastWriter out = new FastWriter();
+
+    static {
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            try {
+                System.setOut(new PrintStream(
+                        new FileOutputStream("output.txt")));
+                System.setIn(new FileInputStream("input.txt"));
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            int testCases = in.nextInt();
+            while (testCases-- > 0) {
+                solve();
+            }
+            out.close();
+        } catch (Exception e) {
+        }
+
+    }
+
+    public static void solve() {
+
+    }
+}
