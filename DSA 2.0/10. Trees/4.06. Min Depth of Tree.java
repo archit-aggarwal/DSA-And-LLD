5c8820ee1b3fb5231161e9933ec8bc2d3c4c56db
archit-aggarwal <archit.aggarwal023@gmail.com>
2023-02-06 23:18:12 +0530

    Added Codes

--- DSA 2.0/10. Trees/4.06. Min Depth of Tree.java
+++ DSA 2.0/10. Trees/4.06. Min Depth of Tree.java
@@ -36,3 +36,17 @@ class Solution {
         return Math.min(lh, rh) + 1;
     }
 }
+
+class Solution2 {
+    public int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int lh = minDepth(root.left);
+        int rh = minDepth(root.right);
+        if (lh == 0)
+            return rh + 1;
+        if (rh == 0)
+            return lh + 1;
+        return Math.min(lh, rh) + 1;
+    }
+}
