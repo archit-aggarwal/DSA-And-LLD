8c01598a6ed1dafd54d559fae368d403454433de
archit-aggarwal <archit.aggarwal023@gmail.com>
2023-02-09 21:48:16 +0530

    Added Codes

--- DSA 2.0/10. Trees/4.07. Balanced Binary Tree.java
+++ DSA 2.0/10. Trees/4.07. Balanced Binary Tree.java
@@ -70,3 +70,29 @@ class Solution2 {
         return ans.isBalanced;
     }
 }
+
+// Brute Force
+// Time = O(N ^ 2), Space = O(N)
+class Solution {
+    public int height(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        return Math.max(lh, rh) + 1;
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        if (Math.abs(lh - rh) > 1)
+            return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+}
