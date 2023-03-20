9d74e168faf4b52ee46e83dfb1551c452a1d56c9
archit-aggarwal <archit.aggarwal023@gmail.com>
2023-03-20 23:00:55 +0530

    added codes

--- DSA 2.0/13.PriorityQueue/6.05. Merge Intervals.java
+++ DSA 2.0/13.PriorityQueue/6.05. Merge Intervals.java
@@ -78,3 +78,49 @@ class Solution2 {
         return res;
     }
 }
+
+// Approach 3
+
+class Solution {
+    static class Pair implements Comparable<Pair> {
+        int start, end;
+
+        Pair(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public int compareTo(Pair other) {
+            if (this.start < other.start)
+                return -1;
+            else if (this.start > other.start)
+                return +1;
+            return 0;
+        }
+    }
+
+    public int[][] merge(int[][] intervals) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for (int[] interval : intervals)
+            pq.add(new Pair(interval[0], interval[1]));
+
+        List<Pair> res = new ArrayList<>();
+        while (pq.size() > 0) {
+            Pair curr = pq.remove();
+            Pair last = (res.size() == 0) ? null
+                    : res.get(res.size() - 1);
+
+            if (last == null || last.end < curr.start)
+                res.add(curr);
+            else
+                last.end = Math.max(last.end, curr.end);
+        }
+
+        int[][] mat = new int[res.size()][2];
+        for (int i = 0; i < res.size(); i++) {
+            mat[i][0] = res.get(i).start;
+            mat[i][1] = res.get(i).end;
+        }
+        return mat;
+    }
+}
