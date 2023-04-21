6493119b10e55ac93c7eb6ad6e779f27fb6b5b24
archit-aggarwal <archit.aggarwal023@gmail.com>
2023-04-21 16:58:57 +0530

    added codes

---
+++ The DSA Saga/01. Getting Started/01.HelloWorld-I.java
@@ -0,0 +1,10 @@
+// https://practice.geeksforgeeks.org/problems/java-hello-world4004/1
+
+import java.util.*;
+
+class Solution {
+    static void printHelloWorld() {
+        // code here
+        System.out.print("Hello World");
+    }
+}
---
+++ The DSA Saga/01. Getting Started/01.HelloWorld-II.java
@@ -0,0 +1,12 @@
+// https://practice.geeksforgeeks.org/problems/the-new-line-java/1
+
+import java.util.*;
+
+class Geeks {
+    // Function to print each word in single line
+    static void printIndividualLine() {
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+    }
+}
