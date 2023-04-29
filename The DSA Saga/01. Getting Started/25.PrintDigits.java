650521ccb498e01d2542efedf733fd91dc803b7f
archit-aggarwal <archit.aggarwal023@gmail.com>
2023-04-29 10:32:24 +0530

    added codes

---
+++ The DSA Saga/01. Getting Started/25.PrintDigits.java
@@ -0,0 +1,19 @@
+
+// https://www.hackerrank.com/contests/module-1-java-dsa-class-challenges/challenges/print-all-digits-from-end/problem
+import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        while (n != 0) {
+            int digit = n % 10;
+            System.out.println(digit);
+            n /= 10; // n = n / 10
+        }
+
+        scn.close();
+    }
+}
