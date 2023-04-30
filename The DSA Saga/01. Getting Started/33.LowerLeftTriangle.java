0c64cc7be54604a8bb3eac0daef44b62a0a62baa
archit-aggarwal <archit.aggarwal023@gmail.com>
2023-04-30 11:17:19 +0530

    added codes

---
+++ The DSA Saga/01. Getting Started/33.LowerLeftTriangle.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+
+        // Approach 1: Increasing Columns
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Approach 2: Grid Based Approach
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                if (row >= col)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+
+        // Approach 3: Stars & Spaces Approach
+        int stars = 1, spaces = n - 1;
+        for (int row = 1; row <= n; row++) {
+            for (int count = 1; count <= stars; count++) {
+                System.out.print("* ");
+            }
+            for (int count = 1; count <= spaces; count++) {
+                System.out.print("  ");
+            }
+            System.out.println();
+            stars++;
+            spaces--;
+        }
+    }
+}
