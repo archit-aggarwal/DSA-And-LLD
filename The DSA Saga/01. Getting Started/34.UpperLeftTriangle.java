c1c3ac9740cf181d17a2e88967df3fdf1417c293
archit-aggarwal <archit.aggarwal023@gmail.com>
2023-04-30 11:35:13 +0530

    added codes

--- The DSA Saga/01. Getting Started/34.UpperLeftTriangle.java
+++ The DSA Saga/01. Getting Started/34.UpperLeftTriangle.java
@@ -1,16 +1,16 @@
 import java.util.*;
-// https://www.hackerrank.com/contests/module-1-java-dsa-class-challenges/challenges/gkstr18-pattern-4/problem
 
 class Solution {
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         scn.close();
 
         // Approach 1: Increasing Columns
-        for (int row = 1; row <= n; row++) {
+        for (int row = n; row >= 1; row--) {
             for (int col = 1; col <= row; col++) {
-                System.out.print("* ");
+                System.out.print("*");
             }
             System.out.println();
         }
@@ -18,26 +18,26 @@ class Solution {
         // Approach 2: Grid Based Approach
         for (int row = 1; row <= n; row++) {
             for (int col = 1; col <= n; col++) {
-                if (row >= col)
-                    System.out.print("* ");
+                if (n - row + 1 >= col)
+                    System.out.print("*");
                 else
-                    System.out.print("  ");
+                    System.out.print(" ");
             }
             System.out.println();
         }
 
         // Approach 3: Stars & Spaces Approach
-        int stars = 1, spaces = n - 1;
+        int stars = n, spaces = 0;
         for (int row = 1; row <= n; row++) {
             for (int count = 1; count <= stars; count++) {
-                System.out.print("* ");
+                System.out.print("*");
             }
             for (int count = 1; count <= spaces; count++) {
-                System.out.print("  ");
+                System.out.print(" ");
             }
             System.out.println();
-            stars++;
-            spaces--;
+            stars--;
+            spaces++;
         }
     }
 }
